a1c45ef3d895a1f598d265c755c6de53d7e2a6a9
Samuel Sithakoul <66365083+Rubiksman78@users.noreply.github.com>
2023-03-01 11:21:45 +0100

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,4 +1,4 @@
 @echo off
-git fetch origin/main
-git reset --hard origin/main
+git fetch origin
+git reset --hard origin
 "libs/pythonlib/python.exe" -m pip install --force-reinstall -r requirements.txt
